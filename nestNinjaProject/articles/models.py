a187c63bf6769dceedf97b1c9eae551ae167f05c
awiles808 <awiles808@gmail.com>
2018-05-25 23:55:29 -0600

    Added first model  Articles model renamed project and app folder

--- nestNinjaProject/articles/models.py
+++ nestNinjaProject/articles/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Articles(models.Models):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateandTimeFied(auto_now_add=True)
+    # add in thumbnail later
+    # add in author later
