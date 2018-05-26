3c7efd9d1b9aaceb1e6bd06ff2f5813c39eb57ed
awiles808 <awiles808@gmail.com>
2018-05-26 01:36:09 -0600

    Created template tags articles showing up on articles url

--- nestNinja/articles/views.py
+++ nestNinja/articles/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+from.models import Articles
 # Create your views here.
 
 
 def articles_list(request):
-    return render(request, 'articles/article_list.html')
+    articles = Articles.objects.all().order_by('date')
+    return render(request, 'articles/article_list.html', {"articles": articles})
